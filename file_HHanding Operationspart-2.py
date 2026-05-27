af7b311e3ac50b7b90b797178c9dcbbad869b959
kanishyadav0001-creator <kanishyadav0001@gmail.com>
2026-05-27 10:58:43 +0530

    initial commit

---
+++ file_HHanding Operationspart-2.py
@@ -0,0 +1,15 @@
+file_read = open('codingal.txt','r')
+print("File in read Mode -")
+print(file_read.read())
+file_read.close()
+
+file_write = open('codingal.txt','w')
+file_write.write("File in write mode ...")
+file_write.write("Hi! I am Kanish.I am 12 yr. old")
+file_write.close()
+
+file_append = open('codingal.txt', 'a')
+file_append.write("\n File in append mode ....")
+file_append.write("Hi! I am penguin. I am 1 yr. old")
+
+file_append.close()
